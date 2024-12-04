549bce117660c8a8b2fec298ac13a90ae3cfd6f0
Tiagalex <tiagoavrodrigues4@gmail.com>
2024-12-04 14:10:05 +0000

    nothing done

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,41 @@
 /*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:30:20 by tiagalex          #+#    #+#             */
-/*   Updated: 2024/12/03 15:38:17 by tiagalex         ###   ########.fr       */
+/*   Updated: 2024/12/04 14:09:24 by tiagalex         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*fill_line(char *buffer, int fd)
+{
+	ssize_t bytes_read;
+	char	*temp;
+
+
+	bytes_read = read (fd, temp, BUFFER_SIZE);
+	
+}
+
 char	*get_next_line(int fd)
 {
-	ssize_t	
+	static char	*buffer;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+
+	//fill_line (buffer, fd);
+	if(!buffer)
+		return (NULL);
+
+}
+
+int	main()
+{
+	int	fd = open("arquivo.txt", O_RDONLY);
+
+	//get_next_line(fd);
+	
+	close (fd);
+	return (0);
 }
