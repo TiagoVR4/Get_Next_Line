d5825d369f808a1b018a9deccf94b56210ce4879
Tiagalex <tiagoavrodrigues4@gmail.com>
2024-12-11 18:43:51 +0000

    Almost finish (maybe)

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,19 @@
 /*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:30:20 by tiagalex          #+#    #+#             */
-/*   Updated: 2024/12/10 17:04:38 by tiagalex         ###   ########.fr       */
+/*   Updated: 2024/12/11 18:22:39 by tiagalex         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_find_line(char *buffer)
+int	find_line(char *buffer)
 {
 	int	i;
 
 	i = 0;
-	buffer = malloc(sizeof(char) * (ft_strlen(buffer) + 1));
 	if (!buffer)
-		return (NULL);
+		return (0);
 	while (buffer[i] != '\0')
 	{
 		if (buffer[i] == '\n')
@@ -33,25 +32,24 @@ char	*fill_line(char *buffer, int fd)
 {
 	ssize_t	bytes_read;
 	char	*temp;
+	char	*new_buffer;
 
 	bytes_read = 1;
-	while (ft_find_line(buffer) == 0 && bytes_read != 0)
+	while (find_line(buffer) == 0 && bytes_read != 0)
 	{
-		temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		temp = ft_calloc(sizeof(char), (BUFFER_SIZE + 1));
 		if (!temp)
 			return (NULL);
 		bytes_read = read(fd, temp, BUFFER_SIZE);
-		if (bytes_read < 0)
-		{
-			free (temp);
-			free (buffer);
-			return (NULL);
-		}
-		buffer = ft_strjoin (buffer, temp[BUFFER_SIZE]);
-
+		if (bytes_read == -1)
+			return (free(buffer), free(temp), NULL);
+		new_buffer = ft_strjoin(buffer, temp);
+		if (!new_buffer)
+			return (free(new_buffer), free(temp), NULL);
+		free (buffer);
+		buffer = new_buffer;
 	}
-	free (temp);
-	return (buffer);
+	return (free(temp), buffer);
 }
 
 char	*get_next_line(int fd)
@@ -60,10 +58,11 @@ char	*get_next_line(int fd)
 
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
-
-	//fill_line (buffer, fd);
+	buffer = ft_calloc(sizeof(char), (ft_strlen(buffer) + 1));
+	buffer = fill_line(buffer, fd);
 	if(!buffer)
 		return (NULL);
+	return (buffer);
 
 }
 
